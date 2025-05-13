48e7a0c9ad140d2a9ea800329bff837d8c37bb35
BellamAravind0009 <bellamaravind24@gmail.com>
2025-05-13 14:39:41 +0530

    Delete src directory

--- src/utils/validators.py
+++
@@ -1,9 +0,0 @@
-export const validateName = (name) => {
-  const regex = /^[A-Za-z\s\'-]+$/;
-  return regex.test(name);
-};
-
-export const validateAge = (age) => {
-  const ageNum = parseInt(age);
-  return ageNum >= 0 && ageNum <= 120;
-};
